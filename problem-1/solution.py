34cda04da0458c3b891d7434fd19b3579959b726
Ryan Vennell <ryan.vennell@gmail.com>
2012-07-15 22:30:37 -0500

    Adding solutions for euler 1-2, and a blank one for 3

---
+++ problem-1/solution.py
@@ -0,0 +1,14 @@
+"""
+If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
+The sum of these multiples is 23.
+
+Find the sum of all the multiples of 3 or 5 below 1000.
+"""
+
+numbers = []
+
+for x in xrange(1, 1000):
+    if 0 in [x % 3, x % 5]:
+        i += x
+
+print i
