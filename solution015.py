894eb3d48e0a4ceb2ce25e5abacb64f9e6f5d424
Ryan Vennell <ryan.vennell@gmail.com>
2012-07-17 15:59:21 -0500

    Adding solution for 15

--- solution015.py
+++ solution015.py
@@ -1,7 +1,17 @@
 """
-Starting in the top left corner of a 22 grid, there are 6 routes (without backtracking) to the bottom right corner.
+Starting in the top left corner of a 2x2 grid, there are 6 routes (without backtracking) to the bottom right corner.
 
 Image: http://projecteuler.net/project/images/p_015.gif
 
-How many routes are there through a 2020 grid?
+How many routes are there through a 20x20 grid?
 """
+
+from math import factorial
+
+gridWidth = 20
+gridHeight = 20
+
+numerator = factorial(gridWidth+gridHeight)
+denominator = factorial(gridWidth)*factorial(gridHeight)
+
+print numerator/denominator
