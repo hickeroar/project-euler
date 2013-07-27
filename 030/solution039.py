19a8a23e1ff7206d994ed8e5cf8258d6a6462d31
Ryan Vennell <ryan.vennell@gmail.com>
2013-07-27 14:13:12 -0500

    Completing problem 39

--- 030/solution039.py
+++ 030/solution039.py
@@ -6,11 +6,28 @@ If p is the perimeter of a right angle triangle with integral length sides, {a,b
 For which value of p <= 1000, is the number of solutions maximised?
 """
 
-from math import sqrt
+import operator
 
-counts = {}
+squareDict = {}
+squares = []
+successes = {}
 
-for x in xrange(1000, 0, -1):
-    maxInt = float(x**2)
+for x in xrange(1, 1001):
+    square = x**2
+    squares.append(square)
+    squareDict[square] = x
 
-    
+for i in xrange(999, -1, -1):
+    for lower in xrange(i-1, ((i-1)/2), -1):
+
+        a = squares[i] - squares[lower]
+        if a in squareDict:
+            perimeter = squareDict[a] + squareDict[squares[i]] + squareDict[squares[lower]]
+
+            if perimeter <= 1000:
+                if perimeter in successes:
+                    successes[perimeter] += 1
+                else:
+                    successes[perimeter] = 1
+
+print max(successes.iteritems(), key=operator.itemgetter(1))[0]
